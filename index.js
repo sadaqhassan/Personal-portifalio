770ff46e1cd7d2c89a6e8ae1c33cf33052a2bbe4
sadak hassan <sadaqsamor99@gmail.com>
2025-04-19 00:11:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const menuIcon = document.getElementById('MenuIcon');
+const menuList = document.getElementById('menuList');
+
+
+
+
+menuIcon.addEventListener('click',()=>{
+    
+    menuList.classList.toggle('hidden');
+} )
