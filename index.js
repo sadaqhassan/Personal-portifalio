8615a8ce86538de1343f7b624250e8e941e646f4
sadak hassan <sadaqsamor99@gmail.com>
2025-04-19 10:35:44 +0300

    added descriptions

--- index.js
+++ index.js
@@ -19,19 +19,20 @@ const AllProjects =[
     {
         name: 'WebDesigm',
         image: 'images/Webdesign.jpg',
-        phragraph: 'A creative graphic design project'
+        phragraph: 'Skilled web designer with 2 years of experience creating visually stunning and user-friendly websites. Specializing in responsive design, UI/UX, and seamless digital experiences',
+        
         
     }, 
     {
         name: 'GraphicDesign',
         image: 'images/Graphicdesign.jpg',
-        phragraph: 'A creative graphic design project'
+        phragraph: 'Creative graphic designer with  years of experience in branding, logo design, and digital media. Passionate about crafting visually impactful designs that elevate brands and engage audiences',
         
     },
     {
         name: 'FullStack',
         image: 'images/fullstack.jpg',
-        phragraph: 'hello world',
+        phragraph: 'Versatile full-stack developer with 2 years of experience building dynamic, scalable web applications. Proficient in both front-end and back-end technologies, delivering seamless user experiences and robust solutions.',
         
     },
 ];
